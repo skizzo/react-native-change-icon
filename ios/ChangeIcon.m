0563b8356aa7505c2f9cbb3598f93d5628b4a15f
Stephan Müller <stephan.mueller.w@gmail.com>
2020-03-28 00:14:04 +0100

    Don't show Alert (iOS)

--- ios/ChangeIcon.m
+++ ios/ChangeIcon.m
@@ -27,9 +27,28 @@ RCT_REMAP_METHOD(changeIcon, iconName:(NSString *)iconName resolver:(RCTPromiseR
     resolve(@(YES));
 
     // Custom icon
+
+    // Shows Alert on Change
+    /*
     [[UIApplication sharedApplication] setAlternateIconName:iconName completionHandler:^(NSError * _Nullable error) {
         RCTLog(@"%@", [error description]);
     }];
+    */
+
+    // Doesn't Show Alert but uses Private API
+    NSMutableString *selectorString = [[NSMutableString alloc] initWithCapacity:40];
+    [selectorString appendString:@"_setAlternate"];
+    [selectorString appendString:@"IconName:"];
+    [selectorString appendString:@"completionHandler:"];
+
+    SEL selector = NSSelectorFromString(selectorString);
+    IMP imp = [[UIApplication sharedApplication] methodForSelector:selector];
+    void (*func)(id, SEL, id, id) = (void *)imp;
+    if (func)
+    {
+        func([UIApplication sharedApplication], selector, iconName, ^(NSError * _Nullable error) {});
+    }
+
 }
 
 @end
